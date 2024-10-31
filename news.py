da0b63b523a1dd48cb03d2ed7585d31edc9cb2dd
abhay-harpalani <abhay.harpalani@gmail.com>
2024-10-31 14:45:12 -0500

    example flask app

---
+++ news.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index_handler():
+	print("url", request.url)
+	return render_template("index.html", title="Test Article")
+	# return "Hello World!"
+
+if __name__ == "__main__":
+	app.run(debug=False)
